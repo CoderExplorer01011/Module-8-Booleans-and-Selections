4102fa7eaba4f85fa9e8e863066423181b7a205d
David Ramirez <daver3754@gmail.com>
2024-11-17 23:17:18 -0600

    Update Problem3DR.py

--- Problem3DR.py
+++ Problem3DR.py
@@ -10,3 +10,5 @@ if 5 in List1:
     print("5 is in the list")
 else:
     print("5 is not in the list")
+
+#This program generates a list and a if statement states whether the number 5 is in that list
